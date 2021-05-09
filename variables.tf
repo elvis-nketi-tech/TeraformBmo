b13014dfefc1303d93ec4fc25e0e7031a8f98aab
Elvis <elvisnketi2017@gmail.com>
2021-05-09 01:00:50 -0400

    nothing serious

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-    default = "us-west-2"
+    default = "us-east-1"
     description = "eks cluster for our client would be provisioned above"
 
     variable "cluster-name" {
